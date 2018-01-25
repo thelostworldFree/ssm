cca2cfc4d9a5300e721dccd31670f2ad25c231df
thelostworld <15244906950@163.com>
2018-01-25 15:28:04 +0800

    first commit4

--- src/com/fuwei/controller/CategoryController.java
+++ src/com/fuwei/controller/CategoryController.java
@@ -4,21 +4,25 @@ package com.fuwei.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.fuwei.aop.ControllerAspect;
 import com.fuwei.pojo.Category;
 import com.fuwei.service.CategoryService;
 import com.fuwei.util.Page;
 
 // 告诉spring mvc这是一个控制器类
+@Component("s")
 @Controller
 @RequestMapping("")
 public class CategoryController {
 	@Autowired
 	CategoryService categoryService;
+	
 
 	@RequestMapping(value = "listCategory", method = RequestMethod.GET )
 	public ModelAndView listCategory(Page page){
